3a8f2355732eb8d037a392e61418672cf2a2b4a6
Muditha Pradeeptha <it21235924@my.sliit.lk>
2023-10-29 05:22:45 +0530

    qr complete function

--- app/src/main/java/com/example/csse_mobile/QRScanActivity.kt
+++ app/src/main/java/com/example/csse_mobile/QRScanActivity.kt
@@ -1,18 +1,30 @@
 package com.example.csse_mobile
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.csse_mobile.databinding.ActivityQrscanBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
+import java.util.UUID
+
 
 class QRScanActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val db = Firebase.firestore
 
     private val requestPermissionlauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
@@ -38,8 +50,85 @@ class QRScanActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQrscanBinding
 
-    private fun setResult(string: String){
-        binding.textResult.text = string
+    private fun setResult(place: String){
+        binding.textResult.text = place
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val userID = FirebaseAuth.getInstance().currentUser!!.uid
+
+        val ref = db.collection("Trip")
+
+        ref.get().addOnSuccessListener { querySnapshot ->
+            if (querySnapshot.isEmpty) {
+                val tripID = UUID.randomUUID().toString()
+                val userID = FirebaseAuth.getInstance().currentUser!!.uid
+                val tripMap = hashMapOf(
+                    "User_ID" to userID,
+                    "Trip_ID" to tripID,
+                    "startDestination" to place,
+                    "startTime" to FieldValue.serverTimestamp(),
+                    "endDestination" to null,
+                    "endTime" to null,
+                )
+
+                db.collection("Trip").document(userID).set(tripMap).addOnSuccessListener {
+                    Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+
+                }
+            }
+            else{
+                db.collection("Trip").orderBy("startTime", Query.Direction.DESCENDING).limit(1).get().addOnSuccessListener { querySnapshot ->
+                    val tripDoc = querySnapshot.first()
+
+                    if(tripDoc["endTime"] == null) {
+
+                        db.collection("Trip").document(userID).update("endDestination",place)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "Profile updated", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener {exception ->
+                                Log.d("EditProfile", "update failed with ", exception)
+                            }
+
+                        db.collection("Trip").document(userID).update("endTime",FieldValue.serverTimestamp())
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "Profile updated", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener {exception ->
+                                Log.d("EditProfile", "update failed with ", exception)
+                            }
+
+                    }
+                    else{
+                        val tripID = UUID.randomUUID().toString()
+                        val userID = FirebaseAuth.getInstance().currentUser!!.uid
+                        val tripMap = hashMapOf(
+                            "User_ID" to userID,
+                            "Trip_ID" to tripID,
+                            "startDestination" to place,
+                            "startTime" to FieldValue.serverTimestamp(),
+                            "endDestination" to null,
+                            "endTime" to null,
+                        )
+
+                        db.collection("Trip").document(userID).set(tripMap).addOnSuccessListener {
+                            Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
+
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+
+                        }
+                    }
+
+                }
+            }
+        }
+
+
     }
 
     private fun showCamera() {
